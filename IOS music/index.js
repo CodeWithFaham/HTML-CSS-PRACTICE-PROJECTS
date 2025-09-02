af30c5c101af687669ced583c529ce89688182fd
Muhammad Faham <fahambbs@gmail.com>
2025-09-02 01:12:30 -0700

    Add files via upload

---
+++ IOS music/index.js
@@ -0,0 +1,11 @@
+// const playlist = document.querySelector("playlist");
+// const tracks = playlist.querySelector("audio");
+// for (let i = 0; i < tracks.length; i++) {
+// 	tracks[i].addEventListener("play", function() {
+// 		for (let j = 0; j < tracks.length; j++) {
+// 			if (tracks[j] != this) {
+// 				tracks[j].pause();
+// 			}
+// 		}
+// 	});
+// }
